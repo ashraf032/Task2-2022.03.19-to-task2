1162d29af6771ebf996d854fb74ffe555146c5e4
ashraf032 <101041982+ashraf032@users.noreply.github.com>
2022-03-25 19:03:54 +0400

    Result commit

---
+++ 3reqemli/3reqemli/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _3reqemli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir 2 reqemli eded daxil edin");
+
+            Console.WriteLine("------------------");
+
+            string num = Console.ReadLine();
+
+            int a = int.Parse(num);
+
+            if (a>=10 && a<100)
+
+            {
+
+                Console.WriteLine("Netice");
+
+                Console.WriteLine("------------");
+
+                Console.WriteLine($"{a}{a}");
+
+            }
+            else
+            {
+                Console.WriteLine("---------------");
+
+                Console.WriteLine("Nezere alinmayib");
+            }
+        }
+    }
+}
